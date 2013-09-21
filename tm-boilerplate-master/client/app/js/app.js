e4db44a106e904d5e6c603e70868015d2be15cab
unknown <abiertasrizzaly@gmail.com>
2013-09-21 12:27:39 +0800

    First Commit

---
+++ tm-boilerplate-master/client/app/js/app.js
@@ -0,0 +1,117 @@
+$(function() {
+
+    var app = {
+        init: function() {
+            this.user = {};
+            $('.menu-crud').addClass('hidden');
+            $('.menu-user').addClass('hidden');
+            $('.menu-loading').removeClass('hidden');
+            $('.menu-user').addClass('hidden');
+            $('.btn-login').addClass('hidden');
+
+            $('.btn-login').attr('href', '/api/login?url=/');
+            $('.btn-logout').attr('href','/api/logout?url=/');
+
+            this.setEventListeners();
+            this.getUser();
+        },
+        setEventListeners: function() {
+            var self = this;
+            $('.menu-crud .item a').click(function(ev) {
+                var $el = $(ev.target).closest('.item');
+
+                $('.menu-crud .item').removeClass('active');
+                $el.addClass("active");
+
+                if ($el.hasClass('menu-list')) {
+                    self.showList();
+                }
+
+                if ($el.hasClass('menu-create')) {
+                    self.showForm();
+                }
+            });
+        },
+        getUser: function() {
+            var self = this;
+            $.ajax({
+                method: 'GET',
+                url: '/api/users/me',
+                success: function(me) {
+                    // user is already signed in
+                    console.log(me);
+                    self.user = me;
+                    self.showLogout();
+                },
+
+                error: function(err) {
+                    console.log('you have not authenticated');
+                    self.showLogin();
+                }
+            });
+        },
+        showLogin: function() {
+           $('.menu-loading').addClass('hidden');
+           $('.menu-user').addClass('hidden');
+           $('.btn-login').removeClass('hidden');
+        },
+        showLogout: function() {
+           $('.menu-crud').removeClass('hidden');
+           $('.user-email').text(this.user.email);
+           $('.menu-loading').addClass('hidden');
+           $('.btn-login').addClass('hidden');
+           $('.menu-user').removeClass('hidden');
+        },
+        showList: function() {
+            var $listTemplate = getTemplate('tpl-thesis-list');
+            $('.app-content').html($listTemplate);
+            this.loadAllThesis();
+        },
+        showForm: function(object) {
+            if (!object) {
+                object = {};
+            }
+            var self = this;
+            var $formTemplate = getTemplate('tpl-thesis-form', object);
+            $('.app-content').html($formTemplate);
+
+
+            $('form').unbind('submit').submit(function(ev) {
+                var thesisObject = {};
+                var inputs = $('form').serializeArray();
+                for (var i = 0; i < inputs.length; i++) {
+                    thesisObject[inputs[i].name] = inputs[i].value;
+                }
+                self.save(thesisObject);
+                return false;
+            });
+
+        },
+        loadAllThesis: function() {
+            $.get('/api/thesis', this.displayLoadedList);
+        },
+        displayLoadedList: function(list) {
+            console.log('response', list);
+            //  use tpl-thesis-list-item to render each loaded list and attach it
+
+        },
+        save: function(object) {
+            var self = this;
+
+        }
+
+
+    };
+
+    function getTemplate(template_id, context) {
+        var template, $template, markup;
+        template = $('#' + template_id);
+        $template = Handlebars.compile(template.html());
+        markup = $template(context);
+        return markup;
+
+    }
+
+    app.init();
+
+});
