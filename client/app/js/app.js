1f9b203e4e8a66163e42642b9e2873df894bd578
unknown <abiertasrizzaly@gmail.com>
2013-10-09 18:51:17 +0800

    milestone 1 and 2

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -12,9 +12,13 @@ $(function() {
             $('.btn-login').attr('href', '/api/login?url=/');
             $('.btn-logout').attr('href','/api/logout?url=/');
 
+            this.router = new Router();
             this.setEventListeners();
             this.getUser();
+
+            Backbone.history.start({pushState: true});
         },
+
         setEventListeners: function() {
             var self = this;
             $('.menu-crud .item a').click(function(ev) {
@@ -24,14 +28,19 @@ $(function() {
                 $el.addClass("active");
 
                 if ($el.hasClass('menu-list')) {
-                    self.showList();
+                    self.router.navigate('list', {trigger: true});
                 }
 
                 if ($el.hasClass('menu-create')) {
-                    self.showForm();
+                    self.router.navigate('new', {trigger: true});
                 }
             });
+
+            $('.navbar-brand').click(function() {
+                self.router.navigate('', {trigger: true});
+            });
         },
+
         getUser: function() {
             var self = this;
             $.ajax({
@@ -50,24 +59,33 @@ $(function() {
                 }
             });
         },
-        showLogin: function() {
+        
+		showLogin: function() {
            $('.menu-loading').addClass('hidden');
            $('.menu-user').addClass('hidden');
            $('.btn-login').removeClass('hidden');
         },
-        showLogout: function() {
+        
+		showLogout: function() {
            $('.menu-crud').removeClass('hidden');
            $('.user-email').text(this.user.email);
            $('.menu-loading').addClass('hidden');
            $('.btn-login').addClass('hidden');
            $('.menu-user').removeClass('hidden');
         },
-        showList: function() {
+        
+		showHome: function() {
+            $('.app-content').html('');
+        },
+        
+		showList: function() {
             var $listTemplate = getTemplate('tpl-thesis-list');
             $('.app-content').html($listTemplate);
             this.loadAllThesis();
+			
         },
-        showForm: function(object) {
+        
+		showForm: function(object) {
             if (!object) {
                 object = {};
             }
@@ -87,31 +105,89 @@ $(function() {
             });
 
         },
-        loadAllThesis: function() {
+       
+	   loadAllThesis: function() {
             $.get('/api/thesis', this.displayLoadedList);
         },
-        displayLoadedList: function(list) {
+       
+	   displayLoadedList: function(list) {
             console.log('response', list);
             //  use tpl-thesis-list-item to render each loaded list and attach it
-
+			for (var i = 0; i < list.length; i++){
+				$('.thesis-list').append(getTemplate('tpl-thesis-list-item', list[i]));
+			}
+		$(document).ready(function() {	
+			$('tr').click(function() { 
+			app.router.navigate('thesis-' + $(this).attr('data-id'), {trigger: true});
+				$('.detail-container').empty();
+					var data;
+						for( var i = 0; i < list.length; i++){
+							if($(this).attr('data-id') == list[i].Id){
+								data = i;
+							}
+						}
+				$('.detail-container').append(getTemplate('thesis-detail', list[data]));
+				fbcomment(document, 'script', 'facebook-jssdk');
+			});
+		});
+			
         },
+		
         save: function(object) {
             var self = this;
-
+			$.post('/api/thesis', object);
+			alert("Saved");
         }
-
-
     };
 
+	
     function getTemplate(template_id, context) {
         var template, $template, markup;
         template = $('#' + template_id);
         $template = Handlebars.compile(template.html());
         markup = $template(context);
         return markup;
-
     }
+	
+	function fbcomment(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=576298592434566";
+		  fjs.parentNode.insertBefore(js, fjs);
+		};
 
+    var Router = Backbone.Router.extend({
+        routes: {
+            '': 'onHome',
+            'thesis-:id': 'onView',
+            'new': 'onCreate',
+            'edit': 'onEdit',
+            'list': 'onList'
+        },
+
+       onHome: function() {
+            app.showHome();
+       },
+
+       onView: function(id) {
+		   console.log('thesis id', id);
+       },
+
+       onCreate: function() {
+            app.showForm();
+       },
+
+       onEdit: function() {
+
+       },
+
+       onList: function() {
+            app.showList();
+       }
+
+    });
     app.init();
+	
 
 });
